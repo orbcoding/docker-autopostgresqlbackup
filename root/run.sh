4dad175b694b4edfd0a96faf96331915f6d35fbe
Mikael Norlén <mickenorlen@gmail.com>
2017-10-09 20:32:23 +0200

    Fix run order

--- root/run.sh
+++ root/run.sh
@@ -6,12 +6,13 @@ echo "user           $GMAIL_ACCOUNT" >> /etc/msmtprc
 echo "password       $GMAIL_PASSWORD" >> /etc/msmtprc
 echo "account default : gmail" >> /etc/msmtprc
 
-# start cron
-for crontab in /root/crontabs/*; do /usr/bin/crontab "$crontab"; done
-/usr/sbin/crond -f -l 8
-
 # set timezone
 apk add tzdata
 cp /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
 echo "$TIMEZONE" > /etc/timezone
 apk del tzdata
+
+
+# start cron
+for crontab in /root/crontabs/*; do /usr/bin/crontab "$crontab"; done
+/usr/sbin/crond -f -l 8
